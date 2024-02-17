0c7dd990c8253cf9c964be33959ac3b9b931c11a
m.nechaev <m.nechaev@ritm.media>
2024-02-17 16:55:57 +0300

    init

--- 5Kick_interactiveAnimation/ViewController.swift
+++ 5Kick_interactiveAnimation/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let squareView = UIView()
+    
+    private var animator: UIDynamicAnimator?
+    private var snap: UISnapBehavior?
+    
+    private var tapPosition: CGPoint = .zero
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+                
+        view.addSubview(squareView)
+        
+        squareView.backgroundColor = .systemBlue
+        squareView.layer.cornerRadius = 8
+        
+        squareView.layer.frame = CGRect(
+            x: UIScreen.main.bounds.width / 2 - 50,
+            y: UIScreen.main.bounds.height / 2 - 50,
+            width: 100,
+            height: 100
+        )
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tap))
+        view.addGestureRecognizer(tap)
+        
+        animator = UIDynamicAnimator(referenceView: view)
     }
-
-
+    
+    @objc
+    func tap(_ tap: UITapGestureRecognizer) {
+        if let animator = animator {
+            animator.removeAllBehaviors()
+            let updatedSnap = UISnapBehavior(item: squareView, snapTo: tap.location(in: view))
+            snap = updatedSnap
+            snap?.damping = 2
+            animator.addBehavior(updatedSnap)
+        }
+    }
 }
-
